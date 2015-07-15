840079828c9df82d9fa1a81de8b5192dfba98ae4
Mike McNeil <mike@balderdash.co>
2015-07-15 08:19:24 -0500

    Adds more diagnostic info about the module.

--- bin/kit-pkgversion.js
+++ bin/kit-pkgversion.js
@@ -11,7 +11,7 @@ require('machine-as-script')({
       },
 
       success: {
-        example: '0.0.0'
+        example: {}
       }
 
     },
@@ -19,7 +19,9 @@ require('machine-as-script')({
 
     fn: function (inputs, exits) {
       var path = require('path');
+      var _ = require('lodash');
       var Filesystem = require('machinepack-fs');
+      var NPM = require('machinepack-npm');
 
       Filesystem.readJson({
         source: path.resolve('package.json'),
@@ -39,9 +41,66 @@ require('machine-as-script')({
         },
         // OK.
         success: function(packageMeta) {
-          return exits.success(packageMeta.version);
+
+          var str = JSON.stringify(packageMeta);
+
+          // Parse metadata for the latest version of the NPM package given a package.json string.
+          var npmMetadata = NPM.parsePackageJson({
+            json: str,
+          }).execSync();
+
+          // Parse machinepack data from the provided JSON string.
+          var mpMetadata = {};
+          try {
+            mpMetadata = LocalMachinepacks.parseMachinepackMetadata({
+              json: str,
+            }).execSync();
+          }
+          catch (e){ /* fail silently */ }
+
+          return exits.success(_.merge(npmMetadata, mpMetadata));
         }
       });
     }
   }
-}).exec();
+}).exec({
+  success: function (parsedMetadata){
+    var _ = require('lodash');
+    var chalk = require('chalk');
+
+    console.log('------------------------------------------------------------');
+
+    // Package name  (+machinepack friendly name)
+    console.log(chalk.bold(parsedMetadata.name) + (parsedMetadata.friendlyName?'   ('+parsedMetadata.friendlyName+')':''));
+    // Description
+    if (parsedMetadata.description) {
+      console.log(parsedMetadata.description);
+    }
+    // Version
+    console.log('v'+parsedMetadata.version);
+    // License
+    if (parsedMetadata.license) {
+      console.log(chalk.gray(parsedMetadata.license + ' License'));
+    }
+    else {
+      console.log(chalk.gray('No license specified.'));
+    }
+    console.log();
+
+
+
+    // URLs
+    if (parsedMetadata.npmUrl) {
+      console.log(chalk.underline(parsedMetadata.npmUrl));
+    }
+    if (parsedMetadata.sourceUrl) {
+      console.log(chalk.underline(parsedMetadata.sourceUrl));
+    }
+    if (parsedMetadata.nodeMachineUrl) {
+      console.log(chalk.underline(parsedMetadata.nodeMachineUrl));
+    }
+    console.log('------------------------------------------------------------');
+    console.log();
+
+  }
+});
